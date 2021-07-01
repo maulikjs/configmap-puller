e8b9bc64f4d90d6f266069884ca0ab0baa79451a
erdii <me@erdii.engineering>
2021-07-01 11:09:13 +0200

    push forgotten cleanup
    Signed-off-by: erdii <me@erdii.engineering>

--- cmd/configmap-puller/main.go
+++ cmd/configmap-puller/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"time"
 
 	v1 "k8s.io/api/core/v1"
@@ -15,15 +16,6 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
-	//
-	// Uncomment to load all auth plugins
-	// _ "k8s.io/client-go/plugin/pkg/client/auth"
-	//
-	// Or uncomment to load specific auth plugins
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
 )
 
 var (
@@ -32,6 +24,7 @@ var (
 	configmapNamespace *string
 	configmapKey       *string
 	outfileName        *string
+	resyncPeriod       *string
 )
 
 func main() {
@@ -42,6 +35,7 @@ func main() {
 	configmapNamespace = flag.String("configmap-namespace", "default", "namespace of the configmap to watch")
 	configmapKey = flag.String("configmap-key", "rules.toml", "key of the configmap to read")
 	outfileName = flag.String("outfile-name", "/tmp/rules.toml", "name of the file to write")
+	resyncPeriod = flag.String("resync-period", "5m", "periodic duration after which a configmap resync is forced - uses https://golang.org/pkg/time/#ParseDuration under the hood")
 
 	flag.Parse()
 
@@ -50,7 +44,15 @@ func main() {
 	log.Println("configmapNamespace", *configmapNamespace)
 	log.Println("configmapKey", *configmapKey)
 	log.Println("outfileName", *outfileName)
-	log.Println()
+	log.Println("resyncPeriod", *resyncPeriod)
+	log.Println("==========================")
+	log.Println("run with --help to list all cli flags")
+	log.Println("==========================")
+
+	resyncDuration, err := time.ParseDuration(*resyncPeriod)
+	if err != nil {
+		panic(fmt.Errorf("not able to parse resync-period flag (given value: %s): %w", resyncDuration, err))
+	}
 
 	// load config depending if we are outside or inside a cluster
 	var config *rest.Config
@@ -74,33 +76,38 @@ func main() {
 		panic(err.Error())
 	}
 
-	namespace := *configmapNamespace
-	name := *configmapName
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 
-	ctx := context.Background()
-	errC, dataC, err := watchConfigMap(ctx, clientset, name, namespace)
+	errC, dataC, err := watchConfigMap(ctx, clientset, *configmapName, *configmapNamespace, resyncDuration)
 	if err != nil {
 		panic(err)
 	}
 
+	previousData := ""
+
 	for {
 		select {
 		case <-ctx.Done():
+			log.Println("shutting down")
+			stop()
 			return
 		case err := <-errC:
 			panic(err)
 		case data := <-dataC:
-			log.Println("data:\n", data)
+			if data == previousData {
+				log.Println("data unchanged")
+				continue
+			}
 			if err := writeFile(*outfileName, data); err != nil {
 				panic(err)
 			}
-			log.Println("wrote to file:", *outfileName)
-			log.Println()
+			log.Println("writing", "file", *outfileName)
+			previousData = data
 		}
 	}
 }
 
-func watchConfigMap(ctx context.Context, clientset *kubernetes.Clientset, name, namespace string) (<-chan error, <-chan string, error) {
+func watchConfigMap(ctx context.Context, clientset *kubernetes.Clientset, name, namespace string, resyncDuration time.Duration) (<-chan error, <-chan string, error) {
 	watchSub, err := clientset.CoreV1().ConfigMaps(namespace).Watch(ctx, metav1.SingleObject(
 		metav1.ObjectMeta{
 			Namespace: namespace,
@@ -111,7 +118,7 @@ func watchConfigMap(ctx context.Context, clientset *kubernetes.Clientset, name, 
 		return nil, nil, err
 	}
 
-	ticker := time.NewTicker(1 * time.Minute)
+	ticker := time.NewTicker(resyncDuration)
 
 	errChan := make(chan error)
 	dataChan := make(chan string)
@@ -127,26 +134,22 @@ func watchConfigMap(ctx context.Context, clientset *kubernetes.Clientset, name, 
 					continue
 				}
 				cm := event.Object.(*v1.ConfigMap)
-				dataChan <- getDataFromCM(cm)
+				dataChan <- cm.Data[*configmapKey]
 
 			case t := <-ticker.C:
-				log.Println("tick at:", t)
+				log.Println("tick", "t", t)
 				timeoutCtx, _ := context.WithTimeout(ctx, 10*time.Second)
 				cm, err := clientset.CoreV1().ConfigMaps(namespace).Get(timeoutCtx, name, metav1.GetOptions{})
 				if err != nil {
 					errChan <- err
 					continue
 				}
-				dataChan <- getDataFromCM(cm)
+				dataChan <- cm.Data[*configmapKey]
 			}
 		}
 	}()
 
 	return errChan, dataChan, nil
-}
-
-func getDataFromCM(cm *v1.ConfigMap) string {
-	return cm.Data[*configmapKey]
 }
 
 func writeFile(filename, data string) error {
